a5b989b4011c3c20c19ddc9cfc059d6007553e91
Matheus de Souza Correa <amenetexplayscpu@gmail.com>
2025-09-10 06:16:24 -0300

    Correção dos sliders

--- js/carrosel.js
+++ js/carrosel.js
@@ -1,33 +1,28 @@
 function createSlider() {
     let swippers = []
 
-    if (window.location.pathname == "/index.html") {
-        new Swiper('.swiper', {
-            // Optional parameters
-            direction: 'horizontal',
-            loop: false,
-            parallax: true,
-            autoHeight: true,
-            slidesPerView: 2.5,
-            spaceBetween: 30,
-            allowTouchMove: false,
-          });
-        new Swiper('.swiper2', {
-            // Optional parameters
-            direction: 'horizontal',
-            loop: false,
-            parallax: true,
-            autoHeight: true,
-            slidesPerView: 2.5,
-            spaceBetween: 30,
-            allowTouchMove: true,
-          });
+    new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: false,
+        parallax: true,
+        autoHeight: false,
+        slidesPerView: 2.5,
+        spaceBetween: 30,
+        allowTouchMove: false,
+      });
+    new Swiper('.swiper2', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: false,
+        parallax: true,
+        autoHeight: false,
+        slidesPerView: 2.5,
+        spaceBetween: 30,
+        allowTouchMove: true,
+      });
 
-          swippers = document.querySelectorAll(".swiper")
-    } 
-    if (window.location.pathname) {
-        //Adicionar mais uma criação de sliders
-    }
+      swippers = document.querySelectorAll(".swiper")
 
     swippers.forEach((swiper) => {
         let prevButton = swiper.parentNode.querySelector(".button-prev")
