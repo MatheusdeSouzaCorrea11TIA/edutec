b10185ec0eb4d5713392b8f1cea20c637bfb2667
Matheus de Souza Correa - 11TIA <162612190+MatheusdeSouzaCorrea11TIA@users.noreply.github.com>
2025-09-10 07:24:06 -0300

    Mais correções

--- js/jogo.js
+++ js/jogo.js
@@ -4,13 +4,14 @@ const input = document.getElementById("name")
 play.addEventListener("click", (e) => {
     let nextPage = play.href
     let name = input.value
+    let tamanhoNome = name.split("")
     e.preventDefault()
 
-    if (/^[A-Za-z]+$/.test(name)) {
+    if (/^[A-Za-z]+$/.test(name) && tamanhoNome.length <= 10) {
         sessionStorage.setItem("username", name)
         window.location.href = nextPage
         return
     }
 
-    alert("O nome deve ser preenchido, sem caracteres especiais ou numeros")
+    alert("O nome deve ser preenchido, sem caracteres especiais ou numeros com o maximo de 10 caracteres")
 })
