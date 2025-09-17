4ae9d18731b64701b5a652625dbf141e541a1979
Matheus de Souza Correa <amenetexplayscpu@gmail.com>
2025-09-17 17:16:28 -0300

    Correção nas questões do jogo

--- js/zahoot.js
+++ js/zahoot.js
@@ -38,22 +38,25 @@ let questions = [
     {
         desc: "Qual é o malefício em destruir o habitat dos animais e como isso influência negativamente no nosso dia a dia?",
         img: "../assets/questoes/queimadas.jpeg",
-        alternativas: ["Não existe nenhum malefício, porque os animais conseguem se adaptar facilmente em qualquer ambiente, inclusive nas cidades, então isso não afeta em nada o nosso dia a dia." ,"A destruição do habitat dos animais causa desequilíbrio ambiental, reduz a biodiversidade e aumenta o risco de extinção de espécies. Isso impacta diretamente o ser humano, pois compromete a qualidade do ar, da água, da alimentação e até a saúde, já que favorece a propagação de doenças." , "O maior problema de destruir o habitat dos animais é que ficamos sem lugares para construir prédios e casas, o que atrapalha o crescimento das cidades e dificulta o avanço da tecnologia." , "Destruir o habitat dos animais só influencia negativamente porque deixa o planeta mais feio visualmente, já que sem florestas e natureza não temos lugares bonitos para visitar."],
-        correta: 3,
+        alternativas: ["Não existe nenhum malefício, porque os animais conseguem se adaptar facilmente em qualquer ambiente, inclusive nas cidades, então isso não afeta em nada o nosso dia a dia." ,
+            "A destruição do habitat dos animais causa desequilíbrio ambiental, reduz a biodiversidade e aumenta o risco de extinção de espécies. Isso impacta diretamente o ser humano, pois compromete a qualidade do ar, da água, da alimentação e até a saúde, já que favorece a propagação de doenças." , 
+            "O maior problema de destruir o habitat dos animais é que ficamos sem lugares para construir prédios e casas, o que atrapalha o crescimento das cidades e dificulta o avanço da tecnologia." , 
+            "Destruir o habitat dos animais só influencia negativamente porque deixa o planeta mais feio visualmente, já que sem florestas e natureza não temos lugares bonitos para visitar."],
+        correta: 2,
         tempo: 30
     },
     {
         desc: "Qual desses animais é um mamífero marinho?",
         img: "../assets/questoes/mamifero.jpg",
-        alternativas: ["Golfinho", "Pinguim", "Tubarão", "Foca"],
-        correta: 1,
+        alternativas: ["Pinguim","Golfinho", "Tubarão", "Foca"],
+        correta: 2,
         tempo: 10
     },    
     {
         desc: "Qual animal é conhecido por construir represas e alterar ecossistemas?",
         img: "../assets/questoes/represa.jpg",
-        alternativas: ["Castor", "Coelho", "Lontra", "Baleia"],
-        correta: 1,
+        alternativas: ["Baleia", "Coelho", "Lontra", "Castor"],
+        correta: 4,
         tempo: 12
     },
     {
