e1c9789f1e1decc6d6af0f7231ec4514ac497e17
Matheus de Souza Correa - 11TIA <162612190+MatheusdeSouzaCorrea11TIA@users.noreply.github.com>
2025-11-12 07:51:23 -0300

    Começo das mudanças para login

--- js/header.js
+++ js/header.js
@@ -1,36 +1,41 @@
 const body = document.body
 const headerHTML = `<header>
-        <a href="./index.html"><img src="./assets/elementos/logo.svg" alt="Logo" class="logo"></a>
-        <div class="links">
-            <ul class="nav-menu-list">
-                <li class="nav-menu-item">
-                    <a href="./vidaselvagem.html">Vida Selvagem</a>
-                </li>
-                <li class="nav-menu-item">
-                    <a href="./habitats.html">Habitats</a>
-                </li>
-                <li class="nav-menu-item">
-                    <a href="./vidamarinha.html">Vida Marinha</a>
-                </li>
-                <li class="nav-menu-item">
-                    <a href="" id="mais">
-                        Mais
-                        <img src="./assets/images/icons/Arrow.svg" alt="">
-                    </a>
-                </li>
-            </ul>
-        </div>
+    <a href="./index.html"><img src="./assets/elementos/logo.svg" alt="Logo" class="logo"></a>
+    <div class="links">
+        <ul class="nav-menu-list">
+            <li class="nav-menu-item">
+                <a href="./vidaselvagem.html">Vida Selvagem</a>
+            </li>
+            <li class="nav-menu-item">
+                <a href="./habitats.html">Habitats</a>
+            </li>
+            <li class="nav-menu-item">
+                <a href="./vidamarinha.html">Vida Marinha</a>
+            </li>
+            <li class="nav-menu-item">
+                <a href="" id="mais">
+                    Mais
+                    <img src="./assets/images/icons/Arrow.svg" alt="">
+                </a>
+            </li>
+            <li class="nav-menu-item">
+                <a href="./login.html" id="nav-login-button">
+                    Login
+                </a>
+            </li>
+        </ul>
+    </div>
 
-        <div id="popup" class="hiddenPopup">
-            <ul class="nav-popup-list">
-                <li class="nav-popup-item"><a href="./classificacoes.html">Classificações</a></li> <hr>
-                <li class="nav-popup-item"><a href="./curiosidades.html">Curiosidades</a></li> <hr>
-                <li class="nav-popup-item"><a href="./sobrenos.html">Sobre Nós</a></li> <hr>
-                <li class="nav-popup-item"><a href="./jogo.html">Zahoot!</a></li>
-            </ul>
-        </div>
+    <div id="popup" class="hiddenPopup">
+        <ul class="nav-popup-list">
+            <li class="nav-popup-item"><a href="./classificacoes.html">Classificações</a></li> <hr>
+            <li class="nav-popup-item"><a href="./curiosidades.html">Curiosidades</a></li> <hr>
+            <li class="nav-popup-item"><a href="./sobrenos.html">Sobre Nós</a></li> <hr>
+            <li class="nav-popup-item"><a href="./jogo.html">Zahoot!</a></li>
+        </ul>
+    </div>
 
-    </header>`
+</header>`
 
 const footerHTML = `<footer class="rodape">
         <img src="./assets/elementos/logo.svg" alt="logo zurrazard">
